fb6cd347720a6b30cc1cb96e00eeebd7e698c693
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-07 11:24:11 +0530

    init

--- PP/PP-lambda.js
+++ PP/PP-lambda.js
@@ -1,7 +1,15 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+    ddb.get({
+        TableName: 'Invoice',
+        Key: { invoice_number: 'HI', clienId_date: 'Bye' }
+    }, function (err, data) {
+        if (err) {
+            //handle error
+        } else {
+            //your logic goes here
+        }
+    });
 
-    exports.handler = function(event, context, callback){
-
-    jsjkdsjdkskdskd
-
-    }
+}
